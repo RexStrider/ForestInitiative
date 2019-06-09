2f16b0d1c96055eb71d95fff9063e13d66f788bf
Odysseus <arwaterman@gmail.com>
2019-06-08 20:36:37 -0700

    finished back end call to google civic services api

--- controllers/civic_info.js
+++ controllers/civic_info.js
@@ -4,7 +4,8 @@ const keys = require('../config/keys');
 
 exports.getCivicInfo = async function(req, res) {
  try {
-    const URL = `https://www.googleapis.com/civicinfo/v2/representatives?key=${keys.google_civic_info_api}&address=1263%20Pacific%20Ave.%20Kansas%20City%20KS`;
+    const address = req.params.address || 'San Francisco CA';
+    const URL = `https://www.googleapis.com/civicinfo/v2/representatives?key=${keys.google_civic_info_api}&address=${address}`;
 
     const response = await axios.get(URL);
 
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/api/articles/:pageNum', controller.Article.getArticles);
 router.get('/api/articles', controller.Article.getArticles);
 
+router.get('/api/civic_info/:address', controller.CivicInfo.getCivicInfo);
 router.get('/api/civic_info', controller.CivicInfo.getCivicInfo);
 
 router.get('/', (req, res) => res.json({ message: 'main route has been hit!' }))
