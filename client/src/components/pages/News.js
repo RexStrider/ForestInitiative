ed8baf07bc49cd96752b1d9398cf18b57d0b6620
Jessie Sidhu <jsidhu22@gmail.com>
2019-06-08 18:27:36 -0700

    front-end

---
+++ client/src/components/pages/News.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const News = () => {
+    return (
+        <div className='row justify-content-md-center mt-5 text-white text-center'>
+            <div className='col-md-6 border border-white rounded-lg mt-5 h1'>
+                  News!
+            </div>
+        </div>
+    )
+}
+
+export default News;
