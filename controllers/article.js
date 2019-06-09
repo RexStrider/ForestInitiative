7b30aa9f692b655069b3cbce565dec3d40fa5677
Odysseus <arwaterman@gmail.com>
2019-06-08 18:19:20 -0700

    implemented google news api v2, removed scraper

--- controllers/article.js
+++ controllers/article.js
@@ -5,41 +5,55 @@ const cheerio = require("cheerio");
 
 exports.scrapeArticles = async function(req, res) {
     try {
-      let response = await axios.get('http://www.wilderness.org/articles?_limit=7&_page=1&keywords=&_sort=date&_order=desc&type=4%2C9')
-      let $ = cheerio.load(response.data)
+      const pageNum = req.params.pageNum || 1;
 
-      console.log($);
+      const url = 'https://newsapi.org/v2/everything?' +
+          'q=("national park" OR "national parks" OR "state park" OR "state parks") AND (protect OR protected) NOT (kill OR killed OR visit OR "North Face" OR "things to do" OR "ranks best")&' +
+          `page=${pageNum}&` +
+          'apiKey=6e75f816d0b6424e9bff62fee9c8c460';
+      const response = await axios.get(url);
 
-      res.send("scrape route has been hit");
+      // console.log(response);
+      // console.log();
+      // console.log(response.data);
 
-      // TODO: update logic for scraping articles from Wilderness Society
-    //   let element = $(`#stream-panel > div > ol > li`)
+      // let $ = cheerio.load(response.data);
+
+      // console.log();
+      // console.log($);
+      // body > div > div > main > div > 
+      // let element = $(`#solrstrap-hits > div > div.entry`);
+
+      // console.log(element);
+      // console.log();
+      // console.log(element.length);
   
-    //   for(let i=1; i <= element.length; i++) {
-    //     let href = $(`#stream-panel > div > ol > li:nth-child(${i}) a`).attr('href')
-    //     let title = $(`#stream-panel > div > ol > li:nth-child(${i}) h2`).text()
-    //     let summary = $(`#stream-panel > div > ol > li:nth-child(${i}) p`).text()
-    //     let article = new db.Article({
-    //       title: title,
-    //       summary: summary,
-    //       href: href
-    //     });
-    //     await article.save();
-    //   }
-    //   res.render('scraped', {message: "Articles have been scraped"});
+      // for(let i=1; i <= element.length; i++) {
+      //   let href = $(`body > div > div > main > div.components-wrapper > div > div > div.library-landing-wrapper-items > a:nth-child(${i})`).attr('href');
+      //   let title = $(`body > div > div > main > div.components-wrapper > div > div > div.library-landing-wrapper-items > a:nth-child(${i}) > div > h3`).text();
+      //   let summary = $(`body > div > div > main > div.components-wrapper > div > div > div.library-landing-wrapper-items > a:nth-child(${i}) > div > div.wide-teaser-summary > p`).text();
+      //   let article = new db.Article({
+      //     title: title,
+      //     summary: summary,
+      //     href: href
+      //   });
+      //   await article.save();
+      // }
+      // res.json( { message: "Articles successfully scraped", article });
+      // res.json( { message: "scrape article route has been hit" });
+      res.json( {message: "testing news api", body: response.data} );
     } catch(error) {
-      res.render('error', {message: "I'm sorry, an error has occurred. We tried to scrape the articles and failed. This usually occurs when one of the articles getting scrapped has already been saved to the database. I Recommend checking the New York Times articles link below.", error} );
+      res.json({ error });
     }
 }
 
-exports.getArticles = function(req, res) {
-    res.json({message: "get article route has been hit"});
-//   try {
-//     const articles = await db.Article.find({})
-//                                      .populate("notes");
-//     res.render('index', {articles});
-//   }
-//   catch (error) {
-//     res.render('error', {message: "I'm sorry, an error has occurred. We could not retrieve any articles. Try navigating to the home page and clicking the NYT Scraper link.", error} );
-//   }
+exports.getArticles = async function(req, res) {
+    res.json({message: "get article route has been hit" });
+  // try {
+  //   const articles = await db.Article.find({});
+  //   res.json({ articles });
+  // }
+  // catch (error) {
+  //   res.json({ error });
+  // }
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const articleController = require('../controllers/article');
 
 const router = express.Router();
 
-router.get('/api/articles', articleController.getArticles);
+router.get('/api/articles/scrape/:pageNum', articleController.scrapeArticles);
 router.get('/api/articles/scrape', articleController.scrapeArticles);
+router.get('/api/articles', articleController.getArticles);
+router.get('/', (req, res) => res.json({ message: 'main route has been hit!' }))
 
 module.exports = router;
