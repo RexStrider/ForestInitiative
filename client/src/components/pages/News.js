26b3afd3bdb393789aa8127ec8993c080e79e45a
Odysseus <arwaterman@gmail.com>
2019-07-21 21:18:57 -0700

    started changes to news component, adding in the rest of the results

--- client/src/components/pages/News.js
+++ client/src/components/pages/News.js
@@ -4,7 +4,8 @@ import React, { Component } from 'react';
 class News
     extends Component {
     state = {
-        articles: []
+        articles: [],
+        totalResults: 0
     }
 
     getArticles = num => {
@@ -15,7 +16,8 @@ class News
             .then(data => {
                 console.log(data);
                 const articles = data.body.articles;
-                this.setState({ articles });
+                const totalResults = data.body.totalResults;
+                this.setState({ articles, totalResults });
                 console.log(articles);
             })
             .catch(error => {
@@ -29,35 +31,45 @@ class News
 
     render() {
         return (
-            <div className='row justify-content-md-center mt-5 text-white text-center'>
-                <div className='col-md-6 rounded-lg mt-5 p-3 m-4 text-dark'
-                     style={{ backgroundColor: `rgba(255,255,255,.8)` }}>
+            <div>
+                <div className='row justify-content-md-center mt-5 text-white text-center'>
+                    <div className='col-md-6 rounded-lg mt-5 p-3 m-4 text-dark'
+                        style={{ backgroundColor: `rgba(255,255,255,.8)` }}>
 
-                    <h1 className='border-bottom border-dark mb-3 pb-2'>
-                        Learn about the State of the Wild!
-                    </h1>
+                        <h1 className='border-bottom border-dark mb-3 pb-2'>
+                            Learn about the State of the Wild!
+                        </h1>
 
-                    {this.state.articles.map(article => {
-                        return (
-                            <div className='card'>
-                                <div className='card-body'>
-                                    <h4 className='card-title'>
-                                        {article.title}
-                                    </h4>
-                                    { article.author ?
-                                        <p className='card-subtitle mb-2'>by {article.author}</p>
-                                        : <p className='card-subtitle mb-2'>author not found</p> }
-                                    <p className='card-text'>
-                                        {article.description}
-                                    </p>
-                                    <a href={article.url}
-                                       className='card-link'>
-                                        SOURCE: {article.source.name}
-                                    </a>
+                        {this.state.articles.map(article => {
+                            return (
+                                <div className='card'>
+                                    <div className='card-body'>
+                                        <h4 className='card-title'>
+                                            {article.title}
+                                        </h4>
+                                        { article.author ?
+                                            <p className='card-subtitle mb-2'>by {article.author}</p>
+                                            : <p className='card-subtitle mb-2'>author not found</p> }
+                                        <p className='card-text'>
+                                            {article.description}
+                                        </p>
+                                        <a href={article.url}
+                                        className='card-link'>
+                                            SOURCE: {article.source.name}
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className='row justify-content-md-center mt-5 text-white text-center'>
+                    <div className='col-md-6 rounded-lg mt-5 p-3 m-4 text-dark'
+                        style={{ backgroundColor: `rgba(255,255,255,.8)` }}>
+                    
+                    <h1>{ this.state.articles.length > 0 ? Math.ceil(this.state.totalResults / this.state.articles.length) : -1 }</h1>
+
+                    </div>
                 </div>
             </div>
         );
