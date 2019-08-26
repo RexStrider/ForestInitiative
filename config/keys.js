320339a39232d279372ebddb6f66547632e93824
Odysseus <arwaterman@gmail.com>
2019-08-26 10:29:24 -0700

    prepping for production deployment

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+if (Process.env.NODE_ENV === "production") 
+    module.exports = require("./prod.js");
+else
+    module.exports = require("./dev.js");
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    google_news_api: Process.env.google_news_api,
+    google_civic_info_api: Process.env.google_civic_info_api
+};
