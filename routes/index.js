3e0ca72c0847945c55d35bcc4d4ac3e54df6ea8a
Odysseus <arwaterman@gmail.com>
2019-08-26 10:44:06 -0700

    updating api routes

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ const controller = require('../controllers');
 
 const router = express.Router();
 
-router.get('/api/articles/:pageNum', controller.Article.getArticles);
-router.get('/api/articles', controller.Article.getArticles);
+router.get('/articles/:pageNum', controller.Article.getArticles);
+router.get('/articles', controller.Article.getArticles);
 
-router.get('/api/civic_info/:address', controller.CivicInfo.getCivicInfo);
-router.get('/api/civic_info', controller.CivicInfo.getCivicInfo);
+router.get('/civic_info/:address', controller.CivicInfo.getCivicInfo);
+router.get('/civic_info', controller.CivicInfo.getCivicInfo);
 
-router.get('/api/payment', controller.Payment.getPayment);
+router.get('/payment', controller.Payment.getPayment);
 // router.get('/api/payment', controller.Payment.getPayment);
 
-router.get('/', (req, res) => res.json({ message: 'main route has been hit!' }))
+// router.get('/', (req, res) => res.json({ message: 'main route has been hit!' }))
 
 module.exports = router;
